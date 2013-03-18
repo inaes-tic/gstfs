05d3c37e8182fac41c20e31e3fc18b88b6e22893
Niv Sardi <xaiki@evilgiggle.com>
2013-03-18 15:52:14 -0300

    add $3 to all2m4v to be able to set -d on command line
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- all2m4v.sh
+++ all2m4v.sh
@@ -4,7 +4,7 @@ set -x
 
 x264opts="psy-tune=1 speed-preset=9"
 
-./gstfs -f -osrc=$1,dst_ext=m4v,pipeline="filesrc name=\"_source\" ! decodebin name=d \
+./gstfs $3 -f -osrc=$1,dst_ext=m4v,pipeline="filesrc name=\"_source\" ! decodebin name=d \
         { qtmux name=mux ! fdsink name=\"_dest\" sync=false } \
         { d. ! audioconvert ! vorbisenc ! queue ! mux. } \
         { d. ! videoconvert ! x264enc $x264opts ! mux. }" \
