d66b654baa1eb2ad2824da0c15cb3e6ada82057a
Niv Sardi <xaiki@evilgiggle.com>
2013-03-19 11:00:52 -0300

    export flect functions as non-static (breaks linkage)
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- flect.h
+++ flect.h
@@ -1,7 +1,7 @@
 #ifndef _FLECT_H_
 #define _FLECT_H_
 
-static struct fuse_operations *flect_get_fops (void);
+struct fuse_operations *flect_get_fops (void);
 int flect_init (char *path);
 int flect_statfs(const char *path, struct statvfs *stbuf);
 
